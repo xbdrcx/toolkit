c7b7ee4790155d9b26f3d3d665fbcf548d789802
xbdrcx <xbrunocruzx@outlook.pt>
2025-04-25 01:14:53 +0100

    commit

--- src/tools/ColorPicker.tsx
+++ src/tools/ColorPicker.tsx
@@ -1,3 +1,6 @@
+// General
+import { useState } from 'react';
+
 // Icons
 import { TbColorPicker } from "react-icons/tb";
 
@@ -13,21 +16,33 @@ interface Window {
 
 export default function ColorPicker() {
 
+    const [pickedColor, setPickedColor] = useState(null);
+
     const handlePicker = async () => {
         const EyeDropper = (window as any).EyeDropper;
         if (EyeDropper) {
-          const eyeDropper = new EyeDropper();
-          const result = await eyeDropper.open();
-          console.log(result.sRGBHex);
+            const eyeDropper = new EyeDropper();
+            const result = await eyeDropper.open();
+            setPickedColor(result.sRGBHex);
+            console.log(result.sRGBHex);
         } else {
-          alert("EyeDropper not supported");
+            alert("EyeDropper not supported");
         }
     };
 
+    const copyToClipboard = (value: string) => {
+        navigator.clipboard.writeText(value).then(() => {
+            // alert("Copied to clipboard: " + value);
+        }).catch((error) => {
+            console.error('Error copying to clipboard:', error);
+        });
+    }
+
     return (
         <>
-            <h2>Color-Picker & Pallette Generator</h2>
+            <h2>Color-Picker & Palette Generator</h2>
             <div className='colorpicker'>
+                <div className="colorpicker__show" style={{ backgroundColor: pickedColor }}></div>
                 <div className='colorpicker__input'>
                     <label htmlFor="color">Color:</label>
                     <input type="color" id="color" name="color" defaultValue="#000000" />
@@ -46,7 +61,7 @@ export default function ColorPicker() {
                 </div>
                 <div className='colorpicker__output'>
                     <label htmlFor="cmyk">CMYK:</label>
-                    <input type="text" id="cmyk" name="cmyk" defaultValue="0%, 0%, 0%, 100%" />
+                    <input onClick={(e: any) => { copyToClipboard(e.target.value) }} type="text" id="cmyk" name="cmyk" defaultValue="0%, 0%, 0%, 100%" />
                 </div>
                 <button className="btn" onClick={handlePicker}><TbColorPicker /></button>
             </div>
